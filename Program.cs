97b81113adc81c58912080cfa42c5d4efd671725
lost <depsona@gmail.com>
2022-04-02 20:08:52 +0300

    add first method and write readme

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿//Из имеющегося массива сторк формирует массив из строк, длина которыз меньше или равна 3 символа.
+﻿//Из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
+string[] GetArray(string[] firstArray)
+{
+    string[] newArray = new string[firstArray.Length];
+    int realSize = 0;
+    for (var i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            newArray[realSize] = firstArray[i];
+            realSize++;
+        }
+    }
+    Array.Resize(ref newArray, realSize);
+    return newArray;
+}
